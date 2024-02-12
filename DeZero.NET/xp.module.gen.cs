b6aec27ee256d15e44a5aab450b76358934d2656
dhq_boiler <dhq_boiler@live.jp>
2024-02-12 17:54:37 +0900

    now implementing ...

---
+++ DeZero.NET/xp.module.gen.cs
@@ -0,0 +1,23 @@
+﻿using Cupy;
+using Numpy;
+using System.Reflection;
+
+namespace DeZero.NET
+{
+    public static partial class xp
+    {
+        public static void Initialize()
+        {
+            if (Core.GpuAvailable && Core.UseGpu)
+            {
+                var method = typeof(cp).GetMethod("ReInitializeLazySelf", BindingFlags.NonPublic | BindingFlags.Static);
+                method.Invoke(null, []);
+            }
+            else
+            {
+                var method = typeof(np).GetMethod("ReInitializeLazySelf", BindingFlags.NonPublic | BindingFlags.Static);
+                method.Invoke(null, []);
+            }
+        }
+    }
+}
