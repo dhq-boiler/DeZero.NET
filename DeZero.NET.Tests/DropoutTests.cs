d74ede22a8b4f2c884ee852c2226053768a21645
dhq_boiler <dhq_boiler@live.jp>
2024-05-12 00:00:15 +0900

    now tests implementing ...

--- DeZero.NET.Tests/DropoutTests.cs
+++ DeZero.NET.Tests/DropoutTests.cs
@@ -48,13 +48,40 @@ namespace DeZero.NET.Tests
             [Test]
             public void Test_Backward1()
             {
-                //var x_data = xp.random.randn(10, 10).ToVariable();
-                var x_data = xp.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]).ToVariable();
+                var x_data = xp.random.randn(10, 10).ToVariable();
 
                 Variable f(Variable x)
                 {
                     Numpy.np.random.seed(0);
                     return Utils.dropout(x, dropout_ratio: 0.5);
+                }
+
+                Assert.IsTrue(Utils.gradient_check(new Function(args => [f(args.Get<Variable>("x"))]), Params.New.SetPositionalArgs(x_data)));
+            }
+
+            [Test]
+            public void Test_Backward2()
+            {
+                var x_data = xp.random.randn(10, 20).ToVariable();
+
+                Variable f(Variable x)
+                {
+                    Numpy.np.random.seed(0);
+                    return Utils.dropout(x, dropout_ratio: 0.99);
+                }
+
+                Assert.IsTrue(Utils.gradient_check(new Function(args => [f(args.Get<Variable>("x"))]), Params.New.SetPositionalArgs(x_data)));
+            }
+
+            [Test]
+            public void Test_Backward3()
+            {
+                var x_data = xp.random.randn(10, 10).ToVariable();
+
+                Variable f(Variable x)
+                {
+                    Numpy.np.random.seed(0);
+                    return Utils.dropout(x, dropout_ratio: 0.0);
                 }
 
                 Assert.IsTrue(Utils.gradient_check(new Function(args => [f(args.Get<Variable>("x"))]), Params.New.SetPositionalArgs(x_data)));
@@ -77,6 +104,70 @@ namespace DeZero.NET.Tests
             public void Setup()
             {
                 Gpu.Use = false;
+            }
+
+            [Test]
+            public void Test_Forward1()
+            {
+                var x = xp.random.randn(100, 100);
+                var y = Utils.dropout(x.ToVariable(), dropout_ratio: 0.0);
+                var res = Utils.array_equal(y.Data, x);
+                Assert.IsTrue(res);
+            }
+
+            [Test]
+            public void Test_Forward2()
+            {
+                var x = xp.random.randn(100, 100).ToVariable();
+                Variable y;
+                using (DeZero.TestMode())
+                {
+                    y = Utils.dropout(x);
+                }
+                var res = Utils.array_equal(y.Data, x.Data);
+                Assert.IsTrue(res);
+            }
+
+            [Test]
+            public void Test_Backward1()
+            {
+                var x_data = xp.random.randn(10, 10).ToVariable();
+
+                Variable f(Variable x)
+                {
+                    Numpy.np.random.seed(0);
+                    return Utils.dropout(x, dropout_ratio: 0.5);
+                }
+
+                Assert.IsTrue(Utils.gradient_check(new Function(args => [f(args.Get<Variable>("x"))]), Params.New.SetPositionalArgs(x_data)));
+            }
+
+            [Test]
+            public void Test_Backward2()
+            {
+                var x_data = xp.random.randn(10, 20).ToVariable();
+
+                Variable f(Variable x)
+                {
+                    Numpy.np.random.seed(0);
+                    return Utils.dropout(x, dropout_ratio: 0.99);
+                }
+
+                Assert.IsTrue(Utils.gradient_check(new Function(args => [f(args.Get<Variable>("x"))]), Params.New.SetPositionalArgs(x_data)));
+            }
+
+            [Test]
+            public void Test_Backward3()
+            {
+                var x_data = xp.random.randn(10, 10).ToVariable();
+
+                Variable f(Variable x)
+                {
+                    Numpy.np.random.seed(0);
+                    return Utils.dropout(x, dropout_ratio: 0.0);
+                }
+
+                Assert.IsTrue(Utils.gradient_check(new Function(args => [f(args.Get<Variable>("x"))]), Params.New.SetPositionalArgs(x_data)));
             }
         }
     }
