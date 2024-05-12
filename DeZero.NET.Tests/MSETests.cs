c1eab58353efd8471c37b5d739b4d164beb56e8b
dhq_boiler <dhq_boiler@live.jp>
2024-05-12 14:51:17 +0900

    now tests implementing ...

---
+++ DeZero.NET.Tests/MSETests.cs
@@ -0,0 +1,103 @@
+﻿using DeZero.NET.Core;
+using DeZero.NET.Functions;
+using Python.Runtime;
+
+namespace DeZero.NET.Tests
+{
+    public class MSETests
+    {
+        public class cp
+        {
+            [OneTimeSetUp]
+            public void OneTimeSetUp()
+            {
+                if (string.IsNullOrEmpty(Runtime.PythonDLL))
+                {
+                    Runtime.PythonDLL = @"C:\Users\boiler\AppData\Local\Programs\Python\Python38\python38.dll";
+                    PythonEngine.Initialize();
+                }
+            }
+
+            [SetUp]
+            public void Setup()
+            {
+                Gpu.Use = true;
+            }
+
+            [Test]
+            public void Test_Forward1()
+            {
+                var x0 = xp.array([0.0, 1.0, 2.0]).ToVariable();
+                var x1 = xp.array([0.0, 1.0, 2.0]).ToVariable();
+                var expected = ((x0 - x1) * (x0 - x1)).Data.sum() / x0.size;
+                var y = MeanSquaredError.Invoke(x0, x1)[0];
+                Assert.IsTrue(Utils.array_allclose(y.Data, expected));
+            }
+
+            [Test]
+            public void Test_Backward1()
+            {
+                var x0 = xp.random.rand(10).ToVariable();
+                var x1 = xp.random.rand(10).ToVariable();
+                Func<Params, Variable[]> f = args => [MeanSquaredError.Invoke(args.Get<Variable>("x"), x1)[0]];
+                Assert.IsTrue(Utils.gradient_check(new Function(f), Params.New.SetPositionalArgs(x0)));
+            }
+
+            [Test]
+            public void Test_Backward2()
+            {
+                var x0 = xp.random.rand(100).ToVariable();
+                var x1 = xp.random.rand(100).ToVariable();
+                Func<Params, Variable[]> f = args => [MeanSquaredError.Invoke(args.Get<Variable>("x"), x1)[0]];
+                Assert.IsTrue(Utils.gradient_check(new Function(f), Params.New.SetPositionalArgs(x0)));
+            }
+        }
+
+        public class np
+        {
+            [OneTimeSetUp]
+            public void OneTimeSetUp()
+            {
+                if (string.IsNullOrEmpty(Runtime.PythonDLL))
+                {
+                    Runtime.PythonDLL = @"C:\Users\boiler\AppData\Local\Programs\Python\Python38\python38.dll";
+                    PythonEngine.Initialize();
+                }
+            }
+
+            [SetUp]
+            public void Setup()
+            {
+                Gpu.Use = false;
+            }
+
+            [Test]
+            public void Test_Forward1()
+            {
+                var x0 = xp.array([0.0, 1.0, 2.0]).ToVariable();
+                var x1 = xp.array([0.0, 1.0, 2.0]).ToVariable();
+                var expected = ((x0 - x1) * (x0 - x1)).Data.sum() / x0.size;
+                var y = MeanSquaredError.Invoke(x0, x1)[0];
+                Assert.IsTrue(Utils.array_allclose(y.Data, expected));
+            }
+
+            [Test]
+            public void Test_Backward1()
+            {
+                var x0 = xp.random.rand(10).ToVariable();
+                var x1 = xp.random.rand(10).ToVariable();
+                Func<Params, Variable[]> f = args => [MeanSquaredError.Invoke(args.Get<Variable>("x"), x1)[0]];
+                Assert.IsTrue(Utils.gradient_check(new Function(f), Params.New.SetPositionalArgs(x0)));
+            }
+
+            [Test]
+            public void Test_Backward2()
+            {
+                var x0 = xp.random.rand(100).ToVariable();
+                var x1 = xp.random.rand(100).ToVariable();
+                Func<Params, Variable[]> f = args => [MeanSquaredError.Invoke(args.Get<Variable>("x"), x1)[0]];
+                Assert.IsTrue(Utils.gradient_check(new Function(f), Params.New.SetPositionalArgs(x0)));
+            }
+        }
+    }
+}
---
+++ DeZero.NET/Functions/MeanSquaredError.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeZero.NET.Core;
+
+namespace DeZero.NET.Functions
+{
+    public class MeanSquaredError : Function
+    {
+        public override Variable[] Forward(Params args)
+        {
+            var x0 = args.Get<Variable>(0);
+            var x1 = args.Get<Variable>(1);
+            var diff = x0.Data - x1.Data;
+            var y = (diff * diff).sum() / diff.len;
+            return [y.ToVariable(this)];
+        }
+
+        public override Variable[] Backward(Params args)
+        {
+            var gy = args.Get<Variable>(0);
+            var x0 = Inputs.ElementAt(0).Variable;
+            var x1 = Inputs.ElementAt(1).Variable;
+            var diff = x0.Data - x1.Data;
+            var gx0 = gy * diff * (2f / diff.len);
+            var gx1 = -gx0;
+            return [gx0, gx1];
+        }
+
+        public static Variable[] Invoke(Variable x0, Variable x1)
+        {
+            return new MeanSquaredError().Call(Params.New.SetPositionalArgs(x0, x1));
+        }
+    }
+}
