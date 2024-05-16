fb10b39700fd3abbeba01a131b1a78f497ca8d86
dhq_boiler <dhq_boiler@live.jp>
2024-05-16 12:56:34 +0900

    now tests implementing ...

--- DeZero.NET.Tests/SumTests.cs
+++ DeZero.NET.Tests/SumTests.cs
@@ -180,4 +180,164 @@ namespace DeZero.NET.Tests
             }
         }
     }
+    public class SumToTests
+    {
+        [Category("cupy")]
+        public class cp
+        {
+            [OneTimeSetUp]
+            public void OneTimeSetUp()
+            {
+                if (string.IsNullOrEmpty(Runtime.PythonDLL))
+                {
+                    Runtime.PythonDLL = @"C:\Users\boiler\AppData\Local\Programs\Python\Python38\python38.dll";
+                    PythonEngine.Initialize();
+                }
+            }
+
+            [SetUp]
+            public void Setup()
+            {
+                Gpu.Use = true;
+            }
+
+            [Test]
+            public void Test_Forward1()
+            {
+                var x = xp.random.rand(10).ToVariable();
+                var y = SumTo.Invoke(x, new Shape(1))[0];
+                var expected = xp.sum(x.Data);
+                Assert.IsTrue(Utils.array_allclose(y.Data, expected));
+            }
+
+            [Test]
+            public void Test_Forward2()
+            {
+                var x = xp.array([[1f, 2f, 3f], [4f, 5f, 6f]]).ToVariable();
+                var y = SumTo.Invoke(x, new Shape(1, 3))[0];
+                var expected = xp.sum(x.Data, axis: 0, keepdims: true);
+                Assert.IsTrue(Utils.array_allclose(y.Data, expected));
+            }
+
+            [Test]
+            public void Test_Forward3()
+            {
+                var x = xp.random.rand(10).ToVariable();
+                var y = SumTo.Invoke(x, new Shape(10))[0];
+                var expected = x.Data;
+                Assert.IsTrue(Utils.array_allclose(y.Data, expected));
+            }
+
+            [Test]
+            public void Test_Backward1()
+            {
+                var x_data = xp.random.rand(10).ToVariable();
+                Func<Params, Variable[]> f = args => SumTo.Invoke(args.Get<Variable>("x"), new Shape(1));
+                Assert.IsTrue(Utils.gradient_check(new Function(f), Params.New.SetPositionalArgs(x_data)));
+            }
+
+            [Test]
+            public void Test_Backward2()
+            {
+                var x_data = (xp.random.rand(10, 10) * 10).ToVariable();
+                Func<Params, Variable[]> f = args => SumTo.Invoke(args.Get<Variable>("x"), new Shape(10));
+                Assert.IsTrue(Utils.gradient_check(new Function(f), Params.New.SetPositionalArgs(x_data)));
+            }
+
+            [Test]
+            public void Test_Backward3()
+            {
+                var x_data = (xp.random.rand(10, 20, 20) * 100).ToVariable();
+                Func<Params, Variable[]> f = args => SumTo.Invoke(args.Get<Variable>("x"), new Shape(10));
+                Assert.IsTrue(Utils.gradient_check(new Function(f), Params.New.SetPositionalArgs(x_data)));
+            }
+
+            [Test]
+            public void Test_Backward4()
+            {
+                var x_data = xp.random.rand(10).ToVariable();
+                Func<Params, Variable[]> f = args => [SumTo.Invoke(args.Get<Variable>("x"), new Shape(10))[0] + 1];
+                Assert.IsTrue(Utils.gradient_check(new Function(f), Params.New.SetPositionalArgs(x_data)));
+            }
+        }
+
+        [Category("numpy")]
+        public class np
+        {
+            [OneTimeSetUp]
+            public void OneTimeSetUp()
+            {
+                if (string.IsNullOrEmpty(Runtime.PythonDLL))
+                {
+                    Runtime.PythonDLL = @"C:\Users\boiler\AppData\Local\Programs\Python\Python38\python38.dll";
+                    PythonEngine.Initialize();
+                }
+            }
+
+            [SetUp]
+            public void Setup()
+            {
+                Gpu.Use = false;
+            }
+
+            [Test]
+            public void Test_Forward1()
+            {
+                var x = xp.random.rand(10).ToVariable();
+                var y = SumTo.Invoke(x, new Shape(1))[0];
+                var expected = xp.sum(x.Data);
+                Assert.IsTrue(Utils.array_allclose(y.Data, expected));
+            }
+
+            [Test]
+            public void Test_Forward2()
+            {
+                var x = xp.array([[1f, 2f, 3f], [4f, 5f, 6f]]).ToVariable();
+                var y = SumTo.Invoke(x, new Shape(1, 3))[0];
+                var expected = xp.sum(x.Data, axis: 0, keepdims: true);
+                Assert.IsTrue(Utils.array_allclose(y.Data, expected));
+            }
+
+            [Test]
+            public void Test_Forward3()
+            {
+                var x = xp.random.rand(10).ToVariable();
+                var y = SumTo.Invoke(x, new Shape(10))[0];
+                var expected = x.Data;
+                Assert.IsTrue(Utils.array_allclose(y.Data, expected));
+            }
+
+            [Test]
+            public void Test_Backward1()
+            {
+                var x_data = xp.random.rand(10).ToVariable();
+                Func<Params, Variable[]> f = args => SumTo.Invoke(args.Get<Variable>("x"), new Shape(1));
+                Assert.IsTrue(Utils.gradient_check(new Function(f), Params.New.SetPositionalArgs(x_data)));
+            }
+
+            [Test]
+            public void Test_Backward2()
+            {
+                var x_data = (xp.random.rand(10, 10) * 10).ToVariable();
+                Func<Params, Variable[]> f = args => SumTo.Invoke(args.Get<Variable>("x"), new Shape(10));
+                Assert.IsTrue(Utils.gradient_check(new Function(f), Params.New.SetPositionalArgs(x_data)));
+            }
+
+            [Test]
+            public void Test_Backward3()
+            {
+                var x_data = (xp.random.rand(10, 20, 20) * 100).ToVariable();
+                Func<Params, Variable[]> f = args => SumTo.Invoke(args.Get<Variable>("x"), new Shape(10));
+                Assert.IsTrue(Utils.gradient_check(new Function(f), Params.New.SetPositionalArgs(x_data)));
+            }
+
+            [Test]
+            public void Test_Backward4()
+            {
+                var x_data = xp.random.rand(10).ToVariable();
+                Func<Params, Variable[]> f = args => [SumTo.Invoke(args.Get<Variable>("x"), new Shape(10))[0] + 1];
+                Assert.IsTrue(Utils.gradient_check(new Function(f), Params.New.SetPositionalArgs(x_data)));
+            }
+        }
+    }
 }
