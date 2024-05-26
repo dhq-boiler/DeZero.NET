f8e1bc8881c54e00e792803a2a9d495503acca7c
dhq_boiler <dhq_boiler@live.jp>
2024-05-26 20:43:44 +0900

    implemented Sigmoid Layer

---
+++ DeZero.NET/Layers/Sigmoid.cs
@@ -0,0 +1,11 @@
+﻿namespace DeZero.NET.Layers
+{
+    public class Sigmoid : Layer
+    {
+        public override Variable[] Forward(params Variable[] xs)
+        {
+            var x = xs[0];
+            return Functions.Sigmoid.Invoke(x);
+        }
+    }
+}
