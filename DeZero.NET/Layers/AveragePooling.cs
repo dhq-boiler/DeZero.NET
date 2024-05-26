9179df9b1595647812beac8525ff72f9fde2ee30
dhq_boiler <dhq_boiler@live.jp>
2024-05-26 20:45:04 +0900

    organize usings

--- DeZero.NET/Layers/AveragePooling.cs
+++ DeZero.NET/Layers/AveragePooling.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DeZero.NET.Layers
+﻿namespace DeZero.NET.Layers
 {
     public class AveragePooling : Layer
     {
