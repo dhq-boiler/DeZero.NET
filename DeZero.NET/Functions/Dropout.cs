b07b378072bc8a0bf8428c5245bfb96f65885c35
dhq_boiler <dhq_boiler@live.jp>
2024-05-26 20:10:45 +0900

    implemented Dropout Function/Layer

---
+++ DeZero.NET/Functions/Dropout.cs
@@ -0,0 +1,44 @@
+﻿using DeZero.NET.Core;
+using Numpy;
+
+namespace DeZero.NET.Functions
+{
+    public class Dropout : Function
+    {
+        public double DropoutRatio { get; set; }
+        public NDarray Mask { get; set; }
+
+        public Dropout(double dropoutRatio)
+        {
+            this.DropoutRatio = dropoutRatio;
+        }
+
+        public override Variable[] Forward(Params args)
+        {
+            var x = args.Get<Variable>(0);
+            if (Config.Train)
+            {
+                var z = new NDarray(np.random.rand(x.Shape.Dimensions));
+                Mask = z > DropoutRatio;
+                var scale = xp.array(1.0 - DropoutRatio).astype(x.Dtype);
+                var y = x * Mask / scale;
+                return [y];
+            }
+            else
+            {
+                return [x];
+            }
+        }
+
+        public override Variable[] Backward(Params args)
+        {
+            var gy = args.Get<Variable>(0);
+            return [gy * Mask];
+        }
+
+        public static Variable Invoke(Variable x, double dropoutRatio = 0.5)
+        {
+            return new Dropout(dropoutRatio).Call(Params.New.SetPositionalArgs(x))[0];
+        }
+    }
+}
---
+++ DeZero.NET/Layers/Dropout.cs
@@ -0,0 +1,18 @@
+﻿namespace DeZero.NET.Layers
+{
+    public class Dropout : Layer
+    {
+        public double DropoutRatio { get; set; }
+
+        public Dropout(double dropoutRatio)
+        {
+            this.DropoutRatio = dropoutRatio;
+        }
+
+        public override Variable[] Forward(params Variable[] xs)
+        {
+            var x = xs[0];
+            return [Functions.Dropout.Invoke(x, DropoutRatio)];
+        }
+    }
+}
