6d2c9803b1636f33beced20188bb6efb590d58a1
dhq_boiler <dhq_boiler@live.jp>
2024-05-26 21:31:10 +0900

    implemented Flatten Function/Layer

---
+++ DeZero.NET/Functions/Flatten.cs
@@ -0,0 +1,27 @@
+﻿using DeZero.NET.Core;
+
+namespace DeZero.NET.Functions
+{
+    public class Flatten : Function
+    {
+        public Shape OriginalShape { get; set; }
+
+        public override Variable[] Forward(Params args)
+        {
+            var x = args.Get<Variable>(0);
+            this.OriginalShape = x.Shape;
+            return Reshape.Invoke(x, new Shape(x.Shape[0], -1));
+        }
+
+        public override Variable[] Backward(Params args)
+        {
+            var gy = args.Get<Variable>(0);
+            return Reshape.Invoke(gy, this.OriginalShape);
+        }
+
+        public static Variable Invoke(Variable x)
+        {
+            return new Flatten().Call(Params.New.SetPositionalArgs(x))[0];
+        }
+    }
+}
