6b212dd7798c2f36fe976d744fee95bc23619cb4
dhq_boiler <dhq_boiler@live.jp>
2024-05-26 21:19:02 +0900

    implemented InstanceNorm Function/Layer

---
+++ DeZero.NET/Layers/InstanceNorm.cs
@@ -0,0 +1,18 @@
+﻿namespace DeZero.NET.Layers
+{
+    public class InstanceNorm : Layer
+    {
+        public float eps { get; set; }
+     
+        public InstanceNorm(float eps = 1e-8f)
+        {
+            this.eps = eps;
+        }
+
+        public override Variable[] Forward(params Variable[] xs)
+        {
+            var x = xs[0];
+            return [Functions.InstanceNorm.Invoke(x, eps)];
+        }
+    }
+}
