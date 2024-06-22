bd203e41e0bce8507ee68544e1c7eba646619cbd
dhq_boiler <dhq_boiler@live.jp>
2024-06-23 02:02:46 +0900

    refactor

--- DeZero.NET.MNISTSample/Program.cs
+++ DeZero.NET.MNISTSample/Program.cs
@@ -1,99 +1,23 @@
-﻿using System.Diagnostics;
-using System.Text;
-using ClosedXML.Excel;
-
-var max_epoch = 100;
+﻿var max_epoch = 50;
 var batch_size = 100;
 var hidden_size = 1000;
 var enableGpu = true;
-const string xlsx_path = "MNIST_result.xlsx";
 
-Console.OutputEncoding = Encoding.UTF8;
-Process CurrentProcess = default;
+var parentProcess = new ParentProcess(max_epoch, batch_size, enableGpu);
 
-AppDomain.CurrentDomain.ProcessExit += (sender, e) =>
+parentProcess.Fit();
+
+class ParentProcess : DeZero.NET.Processes.ParentProcess
 {
-    if (CurrentProcess is not null)
+    public override string RecordFilePath => "MNIST_result.xlsx";
+    public override string ExecutableAssembly => "DeZero.NET.MNISTSampleWorker.exe";
+
+    public override string ExeArguments(int currentEpoch)
     {
-        CurrentProcess.Kill();
-    }
-};
-
-var processedEpoch = LoadFromExcel(xlsx_path);
-
-foreach (var epoch in Enumerable.Range(processedEpoch, max_epoch))
-{
-    StartProcessAndWait("DeZero.NET.MNISTSampleWorker.exe", $"{epoch + 1} {batch_size} {hidden_size} {enableGpu}");
-}
-
-Console.WriteLine("==================================================================================");
-Console.WriteLine($"{DateTime.Now} Finish training.");
-
-int LoadFromExcel(string mnistResultXlsx)
-{
-    if (!System.IO.File.Exists(mnistResultXlsx))
-    {
-        return 0;
-    }
-    using var workbook = new XLWorkbook(mnistResultXlsx);
-    var worksheet = workbook.Worksheet(1);
-
-    // 1行目の埋まっているセルのうち最も右のセルを取得
-    var lastCell = worksheet.Row(1).CellsUsed().LastOrDefault();
-
-    if (lastCell != null)
-    {
-        var lastCellValue = lastCell.GetValue<string>();
-
-        // セルの値が数字であれば変換して返す
-        if (int.TryParse(lastCellValue, out int currentEpoch))
-        {
-            return currentEpoch;
-        }
+        return $"{currentEpoch} {BatchSize} {1000} {EnableGpu} '{RecordFilePath}'";
     }
 
-    // セルが空または数字でない場合は0を返す
-    return 0;
-}
-
-void StartProcessAndWait(string filename, string arguments, string workingDir = null)
-{
-    var psi = new ProcessStartInfo(filename)
+    public ParentProcess(int max_epoch, int batch_size, bool enableGpu) : base(max_epoch, batch_size, enableGpu)
     {
-        UseShellExecute = false,
-        RedirectStandardOutput = true,
-        RedirectStandardError = true,
-        CreateNoWindow = true,
-        WorkingDirectory = workingDir,
-        Arguments = arguments,
-    };
-    Console.WriteLine($"{DateTime.Now} {(workingDir is null ? Directory.GetCurrentDirectory() : workingDir)}> {filename} {arguments}");
-    CurrentProcess = Process.Start(psi);
-    RedirectStandardOutputToConsole(CurrentProcess);
-    while (!File.Exists("signal"))
-    {
-        Thread.Sleep(1000);
     }
-    File.Delete("signal");
-    CurrentProcess.Kill();
-    CurrentProcess.CancelOutputRead();
-    CurrentProcess = null;
-}
-
-void RedirectStandardOutputToConsole(Process process)
-{
-    process.OutputDataReceived += (sender, e) =>
-    {
-        if (!string.IsNullOrEmpty(e.Data))
-        {
-            Console.CursorVisible = false;
-            Console.WriteLine(e.Data);
-            if (e.Data.EndsWith(" "))
-            {
-                Console.SetCursorPosition(0, Console.CursorTop - 1);
-            }
-        }
-    };
-
-    process.BeginOutputReadLine();
 }
