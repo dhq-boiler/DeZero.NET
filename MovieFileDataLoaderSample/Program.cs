89d52f330f2ab7f56b9fb63c11fccabe302d44a1
dhq_boiler <dhq_boiler@live.jp>
2024-10-20 02:08:48 +0900

    bugfix

--- MovieFileDataLoaderSample/Program.cs
+++ MovieFileDataLoaderSample/Program.cs
@@ -1,6 +1,5 @@
 ﻿var max_epoch = 50;
 var batch_size = 100;
-var hidden_size = 1000;
 var enableGpu = true;
 
 var parentProcess = new ParentProcess(max_epoch, batch_size, enableGpu);
@@ -10,7 +9,7 @@ parentProcess.Fit();
 class ParentProcess : DeZero.NET.Processes.ParentProcess
 {
     public override string RecordFilePath => "MovieFileDataLoaderSample_result.xlsx";
-    public override string ExecutableAssembly => "MovieFileDataLoaderSample.exe";
+    public override string ExecutableAssembly => "MovieFileDataLoaderSampleWorker.exe";
 
     public override string ExeArguments(int currentEpoch)
     {
