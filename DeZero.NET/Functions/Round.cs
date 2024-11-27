7885c0d4a12d1983eab7aa1faf11f4a51ad764f6
dhq_boiler <dhq_boiler@live.jp>
2024-11-27 11:06:05 +0900

    Implemented missing function classes

---
+++ DeZero.NET/Functions/Round.cs
@@ -0,0 +1,28 @@
+﻿using DeZero.NET.Core;
+using DeZero.NET.Extensions;
+
+namespace DeZero.NET.Functions
+{
+    public class Round : Function
+    {
+        public Round()
+        {
+        }
+
+        public override Variable[] Forward(Params args)
+        {
+            var x = args.Get<Variable>(0);
+            return [x.Data.Value.round().ToVariable(this)];
+        }
+
+        public override Variable[] Backward(Params args)
+        {
+            return [new NDarray(0).ToVariable(this)];
+        }
+
+        public static Variable[] Invoke(Variable x)
+        {
+            return new Round().Call(Params.New.SetPositionalArgs(x));
+        }
+    }
+}
---
+++ DeZero.NET/Functions/Subtract.cs
@@ -0,0 +1,29 @@
+﻿using DeZero.NET.Core;
+using DeZero.NET.Extensions;
+
+namespace DeZero.NET.Functions
+{
+    public class Subtract : Function
+    {
+        public Subtract()
+        {
+        }
+
+        public override Variable[] Forward(Params args)
+        {
+            var xs = args.Through.Select(x => x.NDarray).ToArray();
+            return [(xs[0] - xs[1]).ToVariable(this)];
+        }
+
+        public override Variable[] Backward(Params args)
+        {
+            var gy = args.Through[0].Variable;
+            return [gy, -gy];
+        }
+
+        public static Variable[] Invoke(Variable x0, Variable x1)
+        {
+            return new Subtract().Call(Params.New.SetPositionalArgs(x0, x1));
+        }
+    }
+}
